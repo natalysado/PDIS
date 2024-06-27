8473d61375613ee1f4e6e9c61e6ee76fc577862c
natalysado <109217090+natalysado@users.noreply.github.com>
2024-06-26 22:33:04 -0300

    Delete js09/1 directory

--- js09/1/main.js
+++
@@ -1,5 +0,0 @@
-//requiere las funciones sumar y restar que se exportan en el archivo math.js//
-import { sumar, restar } from './math.js';
-//llama a la funcion "sumar y restar" con los valores ayb//
-console.log(sumar(5, 3)); 
-console.log(restar(10, 4)); 
